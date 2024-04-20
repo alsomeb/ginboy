499761314b786e6e623987b9d1832ddb6cbe60e8
alsom <alsomeb@gmail.com>
2024-04-20 13:23:07 +0200

    fileUpload function is now designed to handle errors returned from processFileUpload by sending an appropriate HTTP response with the error message to the client.

--- example.go
+++ example.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"path/filepath"
 	"strings"
@@ -22,31 +24,10 @@ func main() {
 }
 
 func fileUpload(c *gin.Context) {
-	// FormFile returns the first file for the provided form key.
-	file, err := c.FormFile("file")
+	file, err := processFileUpload(c)
+
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "No file!"})
-		// Without the return, the function would continue executing
-		return
-	}
-
-	if !isAllowedExt(file.Filename) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Only JPG and PNG files are allowed"})
-		return
-	}
-
-	// Check if the file size exceeds the maximum allowed size
-	if file.Size > MaxMultipartMem {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "The file size exceeds the maximum limit of 8 MB",
-		})
-		return
-	}
-
-	// Upload the file to specific dst, if same filename exists it will replace it
-	err = c.SaveUploadedFile(file, FileDir+"/"+file.Filename)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
@@ -55,6 +36,33 @@ func fileUpload(c *gin.Context) {
 		"message":  "File uploaded successfully",
 		"filename": file.Filename,
 	})
+}
+
+// processes file and either returns it or a specific error with its error message that we can send in the c.JSON()
+func processFileUpload(c *gin.Context) (*multipart.FileHeader, error) {
+	// FormFile returns the first file for the provided form key.
+	file, err := c.FormFile("file")
+	if err != nil {
+		// %w is a format specifier used within the fmt.Errorf function to indicate that you want to format an error
+		return nil, fmt.Errorf("no file provided")
+	}
+
+	if !isAllowedExt(file.Filename) {
+		return nil, fmt.Errorf("only Jpg, Jpeg and png allowed")
+	}
+
+	// Check if the file size exceeds the maximum allowed size
+	if file.Size > MaxMultipartMem {
+		return nil, fmt.Errorf("the file size exceeds the maximum limit of 8 MB")
+	}
+
+	// Upload the file to specific dst, if same filename exists it will replace it
+	err = c.SaveUploadedFile(file, FileDir+"/"+file.Filename)
+	if err != nil {
+		return nil, fmt.Errorf("processing file err: %w", err)
+	}
+
+	return file, nil
 }
 
 func isAllowedExt(filename string) bool {
