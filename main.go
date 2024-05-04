f92dcff1d4f4533cb1718733078e5026c195815a
alsom <alsomeb@gmail.com>
2024-05-04 15:36:24 +0200

    - changed name of data package to fileupload

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"ginboy/data"
+	"ginboy/upload"
 	"ginboy/utils"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -14,7 +14,7 @@ func main() {
 	router := gin.Default()
 	router.MaxMultipartMemory = utils.MaxMultipartMem
 	router.Static("/photos", utils.FileDir) // This code makes any file in the photos directory accessible via a URL like http://localhost:8080/photos/filename.jpg/png
-	router.POST("/upload", data.FileUpload)
+	router.POST("/upload", upload.FileUpload)
 	err := router.Run(":8080")
 	if err != nil {
 		log.Fatal("Error starting server:", err)
--- upload/fileUpload.go
+++ upload/fileUpload.go
@@ -1,4 +1,4 @@
-package data
+package upload
 
 import (
 	"fmt"
