160249644a95a732a671c3886b48084f6d1cf489
alsom <alsomeb@gmail.com>
2024-05-04 20:55:18 +0200

    - Defer Disconnect() explanation

--- main.go
+++ main.go
@@ -16,7 +16,11 @@ func main() {
 
 	// MongoDB
 	mongoClient := database.InitClient()
-	defer mongoClient.MongoClient.Disconnect(context.TODO()) // Ensure disconnection of MongoDB when main() exits
+
+	// Right after the MongoDB client is initialized, the defer statement is set up. This line doesn't disconnect from MongoDB immediately. Instead, it schedules the Disconnect function to be called later.
+	// When the main function is about to complete execution (which would typically happen if the server shuts down or there's a fatal error causing the program to exit), the defer statement triggers
+	// This calls the Disconnect() function ensuring that MONGODB Connection is cleanly closed
+	defer mongoClient.MongoClient.Disconnect(context.TODO())
 
 	router.POST("/upload", func(c *gin.Context) {
 		upload.FileUpload(c, mongoClient)
